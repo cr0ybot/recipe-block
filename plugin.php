a1fc668ed41b4bd5af36e12312741521c064b52e
Cory Hughart <cory@blackbird.digital>
2020-02-04 22:18:26 -0500

    initial state courtesy of create-guten-block

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Recipe Block
+ * Plugin URI: https://github.com/cr0ybot/recipe-block/
+ * Description: Add recipes to your posts as a Gutenberg block.
+ * Author: Cory Hughart
+ * Author URI: https://coryhughart.com/
+ * Version: 0.1.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package recipe-block
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
